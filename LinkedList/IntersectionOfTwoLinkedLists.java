501102ab648e941e256b88248fd0e87ecc23de59
rohanpandeycs2003 <rohanpandeyte@gmail.com>
2026-09-01 21:23:01 +0530

    Add solution: Intersection of Two Linked Lists (LeetCode, Easy) [LinkedList]

---
+++ LinkedList/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,31 @@
+/*
+Problem: Intersection of Two Linked Lists
+Platform: LeetCode
+Difficulty: Easy
+Topics: Hash Table, Linked List, Two Pointers
+URL: https://leetcode.com/problems/intersection-of-two-linked-lists/
+*/
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
+            }
+
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
+        }
+
+        return p1;
+    }
+}
